df363d7b74ba2acf5b0c8698f88058a9cbe15b90
Ryan Davis <ryan.davis@live.com>
2024-06-12 13:14:46 -0400

    redo action

--- fetch-latest-product2.js
+++ fetch-latest-product2.js
@@ -4,16 +4,25 @@ const fs = require('fs');
 const accessToken = process.env.SHOPIFY_ACCESS_TOKEN;
 const shopifyStore = 'slutforbutt.myshopify.com';
 
-const query = `{
-  products (first: 3) {
+const query = `
+{
+  products(first: 10, sortKey: CREATED_AT, reverse: false, query: "collection:tickets") {
     edges {
       node {
-        id
         title
+        handle
+        images(first: 1) {
+          edges {
+            node {
+              src
+            }
+          }
+        }
       }
     }
   }
-}`
+}
+`;
 
 fetch(`https://${shopifyStore}/admin/api/2021-07/graphql.json`, {
 	method: 'POST',
@@ -21,7 +30,7 @@ fetch(`https://${shopifyStore}/admin/api/2021-07/graphql.json`, {
 		'Content-Type': 'application/graphql',
 		'X-Shopify-Access-Token': accessToken,
 	},
-	body: JSON.stringify({ query }),
+	body: {query},
 })
 	.then(res => res.json())
 	.then(data => {
